60966bc624b085dbd202cc5558decdfcff147ca7
remus <249179149@qq.com>
2021-04-05 18:20:37 +0800

    init client

---
+++ testclient/testclient.cpp
@@ -0,0 +1,64 @@
+﻿// testclient.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+#include "winsock.h"
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <string.h>
+using namespace std;
+
+int main()
+{
+	//初始化WSA
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	if (WSAStartup(sockVersion, &wsaData) != 0)
+	{
+		return 0;
+	}
+
+	//创建套接字
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		printf("socket error !");
+		return 0;
+	}
+
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8888);
+	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+	int conn_res = connect(sock, (sockaddr*)&addr, sizeof(sockaddr));
+	if (conn_res == SOCKET_ERROR) {
+		wprintf(L"connect function failed with error: %ld\n", WSAGetLastError());
+		conn_res = closesocket(sock);
+		if (conn_res == SOCKET_ERROR)
+			wprintf(L"closesocket function failed with error: %ld\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+	else {//conn_res = 0,connect success 
+		printf("connect to server:%s", inet_ntoa(addr.sin_addr));
+	}
+
+	closesocket(sock);
+
+	return 0;
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
